26bf2873358b17b49cb8108d3664040289941cf2
Zaid Zaihan <zaidzaihan1611@gmail.com>
2023-12-21 14:40:50 +0800

    Test 2

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var token;
 
 const express = require('express');
 const app = express();
-const port = 3000; 
+const port = process.env.PORT || 3000; 
 
 const bcrypt = require('bcrypt');
 const saltround = 10;
